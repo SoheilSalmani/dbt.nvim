6cefaaa7cbd08daa4625537957d992a111babf84
Soheil Salmani <salmani.soheil@gmail.com>
2025-09-15 04:11:32 +0200

    Add keymap to print project root

--- lua/dbt.lua
+++ lua/dbt.lua
@@ -1,11 +1,13 @@
 local M = {
+	actions = require("dbt.actions"),
 	config = require("dbt.config"),
+	helpers = require("dbt.helpers"),
 }
 
 function M.setup(config)
 	local merged_config = M.config.merge_with_defaults(config)
 	M.config.validate(merged_config)
-	print(vim.inspect(merged_config))
+	vim.keymap.set("n", "<leader>dr", M.actions.print_project_root, { desc = "dbt.nvim: Print dbt project root" })
 end
 
 return M
