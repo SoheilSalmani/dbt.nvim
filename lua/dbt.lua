78ac5c2141ccfe921064a23dca302fb329daae9f
Soheil Salmani <salmani.soheil@gmail.com>
2025-09-15 02:33:43 +0200

    Update plugin so that it logs configuration

--- lua/dbt.lua
+++ lua/dbt.lua
@@ -4,7 +4,8 @@ local M = {
 
 function M.setup(config)
 	local merged_config = M.config.merge_with_defaults(config)
-	config.validate(merged_config)
+	M.config.validate(merged_config)
+	print(vim.inspect(merged_config))
 end
 
 return M
